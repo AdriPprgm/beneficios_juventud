6426811108c7a529c3d4f6daa60b529209c02957
Adrian Proaño Bernal <140282659+AdriPprgm@users.noreply.github.com>
2025-09-02 18:12:25 -0600

    Commit inicial

---
+++ app/src/androidTest/java/mx/apb/beneficios_juventud/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package mx.apb.beneficios_juventud
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("mx.apb.beneficios_juventud", appContext.packageName)
+    }
+}
